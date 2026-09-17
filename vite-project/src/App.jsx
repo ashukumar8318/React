a8adf6221cdc0f73b3704a6cb5e687b7e6676557
ashukumar8318 <ashu.kumarag1998@gmail.com>
2026-09-17 14:16:32 +0530

    Add UserInput component and integrate it into App

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import Welcome from './component/Welcome'
 import Product from './component/Product'
+import UserInput from './component/UserInput'
 
 
 function App(){
   return(
+    <>
      <Product name ="ashu"/>
+     <UserInput/>
 
+    </>
   )
 }
 
---
+++ vite-project/src/component/UserInput.jsx
@@ -0,0 +1,40 @@
+
+
+// function UserInput(){
+
+import { useState } from "react"
+
+//     function handleChange(e){
+//         console.log(e.target.value)
+
+//     }
+
+
+//     return(
+//         <>
+//         <input onChange={handleChange} type="text" />
+//         </>
+//     )
+// }
+
+
+
+function UserInput(){
+    const [name, setName] = useState("")
+
+        function submitChange(e){
+            setName(e.target.value)
+
+
+        }
+      
+        return(
+        <>
+        <input type="text" onChange={submitChange} />
+        <h1>Hello {name}</h1>
+        </>
+    )
+    
+}
+
+export default UserInput
